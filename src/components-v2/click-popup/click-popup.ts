8743f110a336c6915a2e23485773dfb413e2f6c1
Emily Chiang <emily_chiang@ico.com.hk>
2018-10-05 11:10:59 +0800

    commit for click-popup

--- src/components-v2/click-popup/click-popup.ts
+++ src/components-v2/click-popup/click-popup.ts
@@ -57,7 +57,7 @@ export class ClickPopupComponent implements OnInit, OnChanges {
         }
         
         if (component.callback) {
-            component.callback();
+            component.callback(data);
         }
     }
 
